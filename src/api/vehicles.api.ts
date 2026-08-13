a50d8d28fbab0ce402eff443c7d56fdfe9291c25
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-08-12 19:08:23 -0500

    first commit

---
+++ src/api/vehicles.api.ts
@@ -0,0 +1,29 @@
+import { http } from "./http";
+    
+export type Paginated<T> = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: T[];
+};
+
+export type Vehicles = { id: number; nombre: string };
+
+export async function listVehiclessApi() {
+  const { data } = await http.get<Paginated<Vehicles>>("/api/vehicles/");
+  return data; // { count, next, previous, results }
+}
+
+export async function createVehiclesApi(nombre: string) {
+  const { data } = await http.post<Vehicles>("/api/vehicles/", { nombre });
+  return data;
+}
+
+export async function updateVehiclesApi(id: number, nombre: string) {
+  const { data } = await http.put<Vehicles>(`/api/vehicles/${id}/`, { nombre });
+  return data;
+}
+
+export async function deleteVehiclesApi(id: number) {
+  await http.delete(`/api/Vehicless/${id}/`);
+}
